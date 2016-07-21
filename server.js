8d08c4a2446a315186aec2a48bf96b4a4a39e827
Nick Phillips <phillipn101@gmail.com>
2016-07-21 17:28:31 -0500

    Complete

---
+++ server.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+function parseTime(time) {
+  var regToUnix = Date.parse(time);
+  var unixToReg = new Date(+time);
+  
+  if(!time.match(/^\d+$/) && regToUnix){
+    time = new Date(time);
+    return {
+      natural: months[time.getMonth()] + ' ' + time.getDate() + ', ' + time.getFullYear(),
+      unixtime: regToUnix
+    };
+  } else if (unixToReg !== 'Invalid Date') {
+    return {
+      natural: months[unixToReg.getMonth()] + ' ' + unixToReg.getDate() + ', ' + unixToReg.getFullYear(),
+      unixtime: time
+    };
+  } else {
+    return {
+      natural: null,
+      unixtime: null
+    };
+  }
+}
+
+app.get('/:date?', function(req, res){
+  var params = req.params.date;
+  if(!params){
+    res.render('home');
+  } else {
+    if(params !== 'favicon.ico'){
+      res.end(JSON.stringify(parseTime(params)));
+    }
+  }
+})
+
+var server = http.createServer(app).listen(8080, function(){
+  console.log("listening...");
+});
